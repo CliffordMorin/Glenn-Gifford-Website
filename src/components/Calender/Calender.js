b8a2e17748d38d3f2792bccc5fc1f6cfd59c741c
Clifford Morin <cemorin21@gmail.com>
2022-03-07 21:45:23 -0500

    added calendar to calendar page, no success with google api, added more pictures

--- src/components/Calender/Calender.js
+++ src/components/Calender/Calender.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { React, useEffect } from "react";
+import { Button, Typography } from "@mui/material";
 
 const Calender = () => {
-  return <div>Calender</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: "40px",
+      }}
+    >
+      <iframe
+        title="calender"
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&mode=WEEK&showTabs=1&showPrint=0&showTitle=0&showCalendars=1&showTz=1&showDate=1&showNav=1&src=Y2xpZmZvcmRtb3Jpbm11c2ljQGdtYWlsLmNvbQ&color=%23039BE5"
+        width="800"
+        height="600"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+  );
 };
 
 export default Calender;
--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -3,11 +3,41 @@ import ImageGallery from "react-image-gallery";
 import glenn1 from "../../images/glenn1.jpeg";
 import glenn2 from "../../images/glenn2.jpg";
 import glenn3 from "../../images/glenn3.jpg";
+import glennTMA from "../../images/glennTMA.jpg";
+import group from "../../images/group.jpg";
+import head from "../../images/head.jpg";
+import head2 from "../../images/head2.jpg";
+import head3 from "../../images/head3.jpg";
+import head4 from "../../images/head4.jpg";
 import makeStyles from "./styles";
 import { Container } from "@mui/material";
 import "./Media.css";
 
 const images = [
+  {
+    original: glennTMA,
+    thumbnail: glennTMA,
+  },
+  {
+    original: group,
+    thumbnail: group,
+  },
+  {
+    original: head,
+    thumbnail: head,
+  },
+  {
+    original: head2,
+    thumbnail: head2,
+  },
+  {
+    original: head3,
+    thumbnail: head3,
+  },
+  {
+    original: head4,
+    thumbnail: head4,
+  },
   {
     original: glenn1,
     thumbnail: glenn1,
