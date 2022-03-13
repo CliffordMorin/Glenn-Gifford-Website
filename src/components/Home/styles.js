592bc927d74ae7724dd8e948ece715ad071bc254
Clifford Morin <cemorin21@gmail.com>
2022-03-12 19:21:29 -0500

    trying to make footer stick to the bottom of the page and keep the home page image responsive but still be fairly big on mobile view

--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,16 +1,7 @@
 import { makeStyles } from "@mui/styles";
 
 export default makeStyles((theme) => ({
-  homeTxt: {},
   images: {
     width: "100%",
-  },
-  text: {
-    color: "#fff",
-    padding: "20px",
-  },
-  image: {
-    width: "auto",
-    height: "100vh",
   },
 }));
