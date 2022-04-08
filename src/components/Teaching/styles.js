69012609cfb5bc263c72b3032e11fbe07f695b4f
Clifford Morin <cemorin21@gmail.com>
2022-04-08 15:06:10 -0400

    new teaching with testimonials

--- src/components/Teaching/styles.js
+++ src/components/Teaching/styles.js
@@ -8,4 +8,10 @@ export default makeStyles((theme) => ({
       textDecoration: "underline",
     },
   },
+  testimonials: {
+    padding: "0px 250px",
+    [theme.breakpoints.down("sm")]: {
+      padding: "0px 20px",
+    },
+  },
 }));
